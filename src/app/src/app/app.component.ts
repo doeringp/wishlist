7913247d3f88d41ac419b3b735f44847699fddd5
doeringp <20394732+doeringp@users.noreply.github.com>
2022-08-12 23:39:20 +0200

    Connected angular app to strapi backend

--- src/app/src/app/app.component.ts
+++ src/app/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'wishlist-app';
+export class AppComponent implements OnInit {
+  wishlistItems: Array<any> = [];
+
+  constructor(
+    private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('api/wishlist-items')
+      .subscribe((res: any) => this.wishlistItems = res.data);
+  }
 }
